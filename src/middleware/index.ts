45d833305dd6f2e52d18878f2e1ff15cf9fc2eda
Aazib Chaudhry <aazibch47@gmail.com>
2023-10-26 16:28:15 +0500

    Create isAuthenticated middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { get, merge } from 'lodash';
+
+import { getUserBySessionToken } from '../models/User';
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies['REST-AUTH'];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
